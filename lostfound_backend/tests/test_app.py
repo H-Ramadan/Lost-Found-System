df48f6b7840ab2ce275e786b50eb755691b97760
H-Ramadan <H.Ramadan2311@nu.edu.eg>
2026-05-15 22:09:15 +0300

    Update test_app.py

--- lostfound_backend/tests/test_app.py
+++ lostfound_backend/tests/test_app.py
@@ -6,12 +6,13 @@ from lostfound_backend.database import db
 def client():
     app = create_app()
     app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:' # قاعدة بيانات في الذاكرة سريعة للتست
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     
     with app.app_context():
-        db.create_all() # بننشئ الجداول هنا
-        yield app.test_client() # هنا التست بيبدأ يشتغل
-        db.drop_all() 
-        def test_server_is_running(client):
-    response = client.get('/')
-    assert response.status_code in [200, 404]
+        db.create_all()
+        yield app.test_client()
+        db.drop_all()
+
+def test_server_is_running(client):
+     response = client.get('/')
+     assert response.status_code in [200, 404]
